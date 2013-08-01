ed9b47a380d601574d2c45f4016f3c47c85f1697
Ronald Rajagukguk <ronald.penalosa@gmail.com>
2013-08-01 21:48:00 +0800

    added test project

---
+++ LibraryTester/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
